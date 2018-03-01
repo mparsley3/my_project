b4b2c88d234cf3f0a84d601408f354a2c7055ecf
Meghan Parsley <parsleymeghan1@gmail.com>
2018-03-01 10:12:30 -0800

    create .py script

---
+++ analysis.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+#saved as analysis.py in my_project folder on Desktop
+
+import pandas as pd
+
+dat = pd.read_csv('data/gapminder_gdp_europe.csv')
